7405898011505d12b48a34285094a643b3be869a
Satyam Toriya <toriyasatyam2@gmail.com>
2022-12-17 19:25:37 +0530

    search bar added in dashboard page

--- Components/Dashboard/Body.js
+++ Components/Dashboard/Body.js
@@ -8,9 +8,9 @@ const Body = ({navigation}) => {
     }
     return<>
     <View style={styles.tabs}>
-        <Text style={styles.tab} onPress={() => categoryFunc('fastFood')}>Fast Food</Text>
-        <Text style={styles.tab} onPress={() => categoryFunc('deserts')}>Deserts</Text>
-        <Text style={styles.tab} onPress={() => categoryFunc('mainCourse')}>Main Course</Text>
+        <Text style={foodContent == "fastFood" ? styles.tabActive : styles.tab} onPress={() => categoryFunc('fastFood')}>Fast Food</Text>
+        <Text style={foodContent == "deserts" ? styles.tabActive : styles.tab} onPress={() => categoryFunc('deserts')}>Deserts</Text>
+        <Text style={foodContent == "mainCourse" ? styles.tabActive : styles.tab} onPress={() => categoryFunc('mainCourse')}>Main Course</Text>
     </View>
     <View>
         <FoodList type={foodContent} navigation={navigation} />
@@ -23,14 +23,22 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-around',
-        padding: 15,
-        backgroundColor: 'rgb(210, 210, 210)'
+        paddingTop: 20,
+        backgroundColor: 'rgb(210, 210, 210)',
     },
     tab: {
         fontSize: 18,
         fontWeight: 'bold',
         letterSpacing: 0.5,
         color: 'rgb(90, 90, 90)',
+    },
+    tabActive: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        letterSpacing: 0.5,
+        color: 'rgb(90, 90, 90)',
+        borderBottomWidth: 5,
+        paddingBottom:15
     }
 });
 export default Body
--- Components/Dashboard/Header.js
+++ Components/Dashboard/Header.js
@@ -1,13 +1,32 @@
-import { View, Text, ImageBackground, StyleSheet } from 'react-native';
-
-const Header = ({navigation}) => {
+import { useState } from 'react';
+import { View, Text, ImageBackground, StyleSheet, TextInput } from 'react-native';
+import data from "../../data.json"
+const Header = ({ navigation }) => {
   const image = require("D:/Tech/React-Native Tutorial/food-corner/assets/home_cover.webp");
+  const [filterData, setFilterData] = useState([])
+  const searchItem = (e) => {
+    const value = e.target.value.toLowerCase()
+    let foodData = []
+    if (value.length > 2) {
+      for (let foodItem in data) {
+        console.log('foodItem', foodItem)
+        let food = data[foodItem].filter((item) => {
+          let itemName = item.name.toLowerCase()
+          return itemName.includes(value)
+          }
+        )
+        foodData = [...foodData, ...food]
+        console.log('food-', food)
+      }
+    }
+    setFilterData(foodData)
+  }
+  console.log('filterData', filterData)
   return <View style={styles.container}>
     <ImageBackground source={image} resizeMode="cover" style={styles.image}>
       <View style={styles.imagebg}>
-        <Text style={styles.text} onPress={() =>
-          navigation.navigate('Detail', { name: 'Jane' })
-        }>Hungry ??</Text>
+        <Text style={styles.text} >Hungry ??</Text>
+        <TextInput style={styles.searchbar} placeholder='Search..' onChange={(e) => searchItem(e)} />
       </View>
     </ImageBackground>
   </View>
@@ -36,17 +55,13 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontFamily: "cursive"
   },
-  tabs: {
-    display:'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    padding: 15,
-    backgroundColor: 'rgb(210, 210, 210)'
-  },
-  tab: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    letterSpacing: 0.5,
-    color: 'rgb(90, 90, 90)',
+  searchbar: {
+    borderColor: "white",
+    borderBottomWidth: 3,
+    borderRadius: 2,
+    paddingLeft: 8,
+    paddingVertical: 8,
+    marginHorizontal: "auto",
+    color: "white"
   }
 });
